e554449e1086698945cbc0447c70dbaf3a329168
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2025-12-11 17:05:48 +0200

    solving problem D task 2

---
+++ Task-2/D_D.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+    }
+
+    if (a[0] == a[1] || a[0] == a[2])
+    {
+      for (int i = 0; i < n; i++)
+      {
+        if (a[i] != a[0])
+        {
+          cout << i + 1 << endl;
+          break;
+        }
+      }
+    }
+    else
+    {
+      for (int i = 0; i < n; i++)
+      {
+        if (a[i] != a[1])
+        {
+          cout << i + 1 << endl;
+          break;
+        }
+      }
+    }
+  }
+}
