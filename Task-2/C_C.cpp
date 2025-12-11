c431bef3dfde3d22eb7e88e776ef56fa3528a9a7
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2025-12-11 17:05:42 +0200

    solving problem C task 2

---
+++ Task-2/C_C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    string s;
+    cin >> s;
+    int l = s.length();
+    if (l <= 10)
+    {
+      cout << s << endl;
+    }
+    else
+    {
+      cout << s[0] << s.length() - 2 << s[l - 1] << endl;
+    }
+  }
+}
