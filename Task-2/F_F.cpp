895c8ea5740a03925019d41bc99602defef53cb2
Mohamed Zakaria <mohamedzakaria532005@gmail.com>
2025-12-11 17:06:04 +0200

    solving problem F task 2

---
+++ Task-2/F_F.cpp
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a[5][5];
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      cin >> a[i][j];
+      if (a[i][j] != 0)
+      {
+        cout << abs(2 - i) + abs(2 - j);
+      }
+    }
+  }
+}
